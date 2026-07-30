ea5803a3f95351d4526e8dfff26f3990da731f7a
Hansika Reddy <hansikareddycheguri@gmail.com>
2026-07-30 22:16:47 +0530

    Updated Railway backend URL

--- client/Evenza/src/services/api.js
+++ client/Evenza/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://evenza-production-e4aa.up.railway.app/api",
+   baseURL:  "https://evenza-production-17ad.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
